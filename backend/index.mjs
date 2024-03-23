f86e9e6699deb6b50058925a297103a9ae71fb30
Hansel Lopez <lopezhansel@gmail.com>
2024-03-23 15:00:48 -0700

    lint

--- backend/index.mjs
+++ backend/index.mjs
@@ -9,16 +9,17 @@ const fastify = Fastify({
 fastify.post('/api/animation/', async function handler(request, reply) {
   return {
     "animationId": "some-uuid-abcd-efgh"
-  }
+  };
 })
 
 fastify.get('/api/animation/:animationId', function (request, reply) {
   const { animationId } = request.params;
+
   return {
     animationId,
     "status": "READY",
     "video": "http:///domain.com/api/animaton/uuid"
-  }
+  };
 })
 
 try {
