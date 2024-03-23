cad7874fb130cae4de23a8d2b56f0203085a5cee
Hansel Lopez <lopezhansel@gmail.com>
2024-03-23 14:00:27 -0700

    setup backend

---
+++ backend/index.mjs
@@ -0,0 +1,29 @@
+import Fastify from 'fastify'
+
+
+// Import the framework and instantiate it
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.post('/api/animation/', async function handler(request, reply) {
+  return {
+    "animationId": "some-uuid-abcd-efgh"
+  }
+})
+
+fastify.get('/api/animation/:animationId', function (request, reply) {
+  const { animationId } = request.params;
+  return {
+    animationId,
+    "status": "READY",
+    "video": "http:///domain.com/api/animaton/uuid"
+  }
+})
+
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
