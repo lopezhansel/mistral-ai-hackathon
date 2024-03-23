cceaf3e1c529d6358b0fd564ba1e171699e13103
Hansel Lopez <lopezhansel@gmail.com>
2024-03-23 16:22:00 -0700

    Add intial state for useQuery

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -2,8 +2,43 @@ import { useState } from "react";
 import ChatMessage from "../ChatMessage/ChatMessage";
 import ChatInput from "../ChatInput/ChatInput";
 import "./Chat.css";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createAnimation, getAnimation } from "../../services/api";
+
+// Access the client
+
+function useCreateAnimation() {
+  const queryClient = useQueryClient();
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: createAnimation,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({
+        queryKey: ["animations"],
+      });
+    },
+  });
+
+  return { mutation };
+}
+
+function useGetAnimation() {
+  const query = useQuery<{ message: string }, string>({
+    queryKey: ["animationss"],
+    queryFn: async (string) => {
+      string.meta;
+      const res = await getAnimation("some");
+      return res;
+    },
+  });
+
+  return { query };
+}
 
 function Chat() {
+  const { mutation } = useCreateAnimation();
   const [messages, setMessage] = useState([
     {
       username: "You",
@@ -13,6 +48,8 @@ function Chat() {
 
   const handleOnSubumit = (message: string) => {
     setMessage([...messages, { message, username: "You" }]);
+    
+    mutation.mutate(message);
   };
 
   return (
