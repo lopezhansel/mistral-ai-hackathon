88f277efca74e0f040bfcfb36d00d2b8204b1f5a
Hansel Lopez <lopezhansel@gmail.com>
2024-03-23 16:20:21 -0700

    Add UUID and cors

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,25 +1,60 @@
 import Fastify from 'fastify'
-
+import cors from '@fastify/cors'
+import { v4 } from 'uuid'
 
 // Import the framework and instantiate it
 const fastify = Fastify({
   logger: true
 })
 
-fastify.post('/api/animation/', async function handler(request, reply) {
-  return {
-    "animationId": "some-uuid-abcd-efgh"
-  };
+await fastify.register(cors, {
+  origin: ['http://localhost:5173']
 })
+
+const map = new WeakMap()
+
+const AnimationStatus = {
+  READY: 'READY',
+  PENDING: 'PENDING',
+  ERROR: 'ERROR'
+}
+
+fastify.decorate('notFound', (request, reply) => {
+  reply.code(404).type('text/html').send('Not Found')
+})
+
+fastify.setNotFoundHandler(fastify.notFound)
+
+
+fastify.post('/api/animation/', async function handler(request, reply) {
+  const { prompt } = JSON.parse(request.body);
+  const animationId = v4();
+
+  const animation = {
+    status: AnimationStatus.PENDING,
+    animationId,
+    prompt
+  }
+
+  map.set(animationId, animation)
+
+  return animation;
+});
 
 fastify.get('/api/animation/:animationId', function (request, reply) {
   const { animationId } = request.params;
 
+  const animation = map.get(animationId)
+
+  if (!animation) {
+    return fastify.notFound(request, reply)
+  }
+
   return {
-    animationId,
-    "status": "READY",
+    ...animation,
+    status: AnimationStatus.READY,
     "video": "http:///domain.com/api/animaton/uuid"
-  };
+  }
 })
 
 try {
