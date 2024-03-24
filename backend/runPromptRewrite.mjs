deb439b46f604733bb532efe82c68195d78af88a
Hansel Lopez <lopezhansel@gmail.com>
2024-03-24 09:35:30 -0700

    Add python script logging

--- backend/runPromptRewrite.mjs
+++ backend/runPromptRewrite.mjs
@@ -1,14 +1,24 @@
+import fs from 'node:fs';
 import { spawn } from 'node:child_process'
+import path from 'node:path';
 
 export function runPromptRewrite(
   prompt = '',
   uuid = ''
 ) {
   return new Promise((resolve, reject) => {
+    const stdOutFile = fs.createWriteStream(
+      path.join(process.cwd(), 'khan-classes', `${uuid}-stdout.log`)
+    );
+    const stdErrFile = fs.createWriteStream(
+      path.join(process.cwd(), 'khan-classes', `${uuid}-stderr.log`)
+    );
     const args = ['./run_prompt_rewrite.sh', `"${prompt}"`, `"${uuid}"`];
     const childProcess = spawn('bash', args, {
       cwd: process.cwd(),
     });
+    childProcess.stdout.pipe(stdOutFile);
+    childProcess.stderr.pipe(stdErrFile);
 
     childProcess.on('exit', (code) => {
       if (code === 0) {
