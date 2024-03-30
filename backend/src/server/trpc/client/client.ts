34b174a73fb8effc3cfd2f1cd215b57b8c17b6e0
Hansel Lopez <lopezhansel@gmail.com>
2024-03-29 22:07:40 -0700

    Add client example

---
+++ backend/src/server/trpc/client/client.ts
@@ -0,0 +1,12 @@
+import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import type { AppRouter } from "../appRouter";
+
+// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
+// what procedures are available on the server and their input/output types.
+export const client = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:2389',
+    }),
+  ],
+});
