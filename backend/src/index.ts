7443e4e3550a1c18234f0e506c71d2b5ed86397c
Hansel Lopez <lopezhansel@gmail.com>
2024-04-01 20:57:52 -0700

    rename server files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
-import { fastify } from './server'
+import { server } from './server'
 
 try {
-  await fastify.listen({ port: 3000 })
+  await server.listen({ port: 3000 })
 } catch (err) {
-  fastify.log.error(err)
+  server.log.error(err)
   process.exit(1)
 }
---
+++ backend/src/server/server.ts
@@ -0,0 +1,24 @@
+import Fastify from 'fastify'
+
+import * as plugins from './plugins';
+import routes from './routes';
+
+export const server = Fastify({
+  logger: true,
+  maxParamLength: 5000,
+});
+
+server.register(plugins.cors);
+await server.register(plugins.staticDir);
+await server.register(plugins.vite);
+server.register(plugins.trpc)
+server.register(routes)
+
+server.decorate('notFound', (request, reply) => {
+  reply.code(404).type('text/html').send('Not Found')
+})
+
+await server.vite.ready();
+
+server.setNotFoundHandler(server.notFound)
+
