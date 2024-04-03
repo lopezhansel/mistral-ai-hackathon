d6feaa3299dfefc623a59f6d9be5c1319343191e
Hansel Lopez <lopezhansel@gmail.com>
2024-04-02 21:36:13 -0700

    fix links

--- frontend/src/lib/trpc.ts
+++ frontend/src/lib/trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from '@trpc/react-query';
-import { AppRouter } from '../../../backend/src/server/trpc/appRouter';
+import { AppRouter } from 'backend/src/server/trpc/appRouter';
 
 export const trpc = createTRPCReact<AppRouter>();
