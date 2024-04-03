3e34f69321dc3e9b13f2c0e5cfbc62c6aae537c0
Hansel Lopez <lopezhansel@gmail.com>
2024-04-03 01:34:52 -0700

    remove old routes

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,10 +1,10 @@
 import Fastify from "fastify";
 
 import * as plugins from "./plugins";
-import routes from "./routes";
 
 export const server = Fastify({
   logger: true,
+  // Larger maxParamLength is needed for trpc query params
   maxParamLength: 5000,
 });
 
@@ -12,6 +12,5 @@ await server.register(plugins.staticDir);
 await server.register(plugins.vite);
 server.register(plugins.trpc);
 server.register(plugins.notFound);
-server.register(routes);
 
 await server.vite.ready();
