e2481b0f93ffbdb05118ae8db65f346ca67ddff0
Hansel Lopez <lopezhansel@gmail.com>
2024-04-03 01:10:28 -0700

    change plugins

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,14 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import config from './tailwind.config.js'
+
+export default ({ env }) => {
+  const plugins = [
+    tailwind(config),
+    env === 'production' ? autoprefixer() : false
+  ];
+
+  return {
+    plugins,
+  };
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import { dirname, resolve } from 'node:path'
 import viteReact from '@vitejs/plugin-react'
 import { fileURLToPath } from 'node:url'
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import config from './tailwind.config.js' 
 
 const pathDir = dirname(fileURLToPath(import.meta.url))
 const frontendDir = resolve(pathDir, './')
@@ -15,15 +12,6 @@ const frontendSrcDir = resolve(pathDir, 'src')
 export default defineConfig({
   root: frontendDir,
   plugins: [viteReact()],
-
-  css: {
-    postcss: {
-      plugins: [
-        tailwind({ config }),
-        autoprefixer(),
-      ]
-    }
-  },
   resolve: {
     alias: {
       '@': frontendSrcDir,
