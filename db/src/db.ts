22b09b2194d3cd2c9e468cf0c8a68799ed053d3d
Hansel Lopez <lopezhansel@gmail.com>
2024-04-04 01:34:17 -0700

    fix import path

--- db/src/db.ts
+++ db/src/db.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "./schema";
