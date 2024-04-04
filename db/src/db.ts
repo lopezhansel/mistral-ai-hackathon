7c652fffdf7bdc9f0b4803715afe0e194765c437
Hansel Lopez <lopezhansel@gmail.com>
2024-04-04 00:03:48 -0700

    remove ts-ignore comment

--- db/src/db.ts
+++ db/src/db.ts
@@ -1,7 +1,4 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
-// tsconfig.allowSyntheticDefaultImports is not enabled
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import Database from "better-sqlite3";
 import * as schema from "./schema";
 import { fileURLToPath } from "url";
