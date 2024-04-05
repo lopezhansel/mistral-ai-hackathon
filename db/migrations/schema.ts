b7499b821e51e9a37b212d4a686aecdd2215c6ab
Hansel Lopez <lopezhansel@gmail.com>
2024-04-05 16:08:15 -0700

    update migration schema

--- db/migrations/schema.ts
+++ db/migrations/schema.ts
@@ -1,6 +1,7 @@
 import { sql } from "drizzle-orm";
 import {
   AnySQLiteColumn,
+  foreignKey,
   integer,
   numeric,
   sqliteTable,
@@ -26,4 +27,23 @@ export const users = sqliteTable("users", {
   phone: numeric("phone"),
   createdAt: text("created_at").default("sql`(CURRENT_TIMESTAMP)`").notNull(),
   intModifiers: integer("int_modifiers").default(false).notNull(),
+});
+
+export const drizzleMigrations = sqliteTable("__drizzle_migrations", {
+  id: numeric("id").primaryKey(),
+  hash: text("hash").notNull(),
+  createdAt: numeric("created_at"),
+});
+
+export const conversations = sqliteTable("conversations", {
+  conversationsId: integer("conversationsId")
+    .primaryKey({ autoIncrement: true })
+    .notNull(),
+  startedByUserId: integer("startedByUserID")
+    .notNull()
+    .references(() => users.id),
+  startedWithUserId: integer("startedWithUserID")
+    .notNull()
+    .references(() => users.id),
+  createdAt: text("created_at").default("sql`(CURRENT_TIMESTAMP)`").notNull(),
 });
