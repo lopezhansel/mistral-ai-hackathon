ca988165fd9226bb3449bf1416d6aa9a96c7b350
Hansel Lopez <lopezhansel@gmail.com>
2024-04-05 15:59:14 -0700

    fix type issue

--- db/src/schema/users.ts
+++ db/src/schema/users.ts
@@ -1,11 +1,11 @@
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, numeric, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: integer("id", { mode: "number" }).primaryKey(),
   firstName: text("first_name"),
   lastName: text("last_name"),
-  phone: text("phone", { length: 256 }),
+  phone: numeric("phone"),
   createdAt: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
   intModifiers: integer("int_modifiers", { mode: "boolean" })
     .notNull()
